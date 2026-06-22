5abe370196caf51c2ce7e819622cf19af84eb449
Ignacio Mucci <biglianiignacio@gmail.com>
2026-06-22 17:41:33 -0300

    Microchip

---
+++ TP3/TP3/Tp3/I2C.c
@@ -0,0 +1,6 @@
+/*
+ * CFile1.c
+ *
+ * Created: 22/6/2026 17:11:07
+ *  Author: rocio
+ */ 
---
+++ TP3/TP3/Tp3/main.c
@@ -0,0 +1,18 @@
+/*
+ * Tp3.c
+ *
+ * Created: 22/6/2026 17:10:22
+ * Author : rocio
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
