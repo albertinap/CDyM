8b26fec55c499c4a078966de061620c44bb1881a
Albertina <alber.pezzutti@gmail.com>
2026-07-29 18:10:34 -0300

    okey

---
+++ TP4/TP4/drivers/uart/CFile1.c
@@ -0,0 +1,6 @@
+/*
+ * CFile1.c
+ *
+ * Created: 29/7/2026 17:48:06
+ *  Author: alber
+ */ 
---
+++ TP4/TP4/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 29/7/2026 17:27:07
+ * Author : alber
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
